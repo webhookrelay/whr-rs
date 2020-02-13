18258398d4534291d939a59c92705a6318575afd
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-02-13 09:55:58 +0000

    updated example

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,8 +1,12 @@
 extern crate whr;
 use whr::{Request};
 
-fn handler(req: Request) {
-    req.set_request_method("DELETE".to_string());
+fn handler(req: Request) {    
+    let mut body = req.get_body();
+    let new_body = format!("{{\"text\": \"{}\"}}", body);
+    
+    whr::set_request_method("PUT".to_string());
+    whr::set_request_body(new_body);
 }
 
 // our entry point into the application
