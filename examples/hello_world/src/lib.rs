19c99c0718452efe7ef94e7f488da90e877ad212
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-02-13 00:02:06 +0000

    example

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate whr;
+// use whr;
+use whr::{Request};
+
+fn handler(req: Request) {
+    req::set_request_method("DELETE")
+}
+
+#[no_mangle]
+pub extern "C" fn handleRequest(ptr: i32, len: i32) {
+    whr::run(ptr, len, handler)
 }
