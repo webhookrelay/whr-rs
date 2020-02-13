fa346adbe70ffbcd475920860ae49bdde4fe53fc
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-02-13 09:37:30 +0000

    req method example

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,11 +1,11 @@
 extern crate whr;
-// use whr;
 use whr::{Request};
 
 fn handler(req: Request) {
-    req::set_request_method("DELETE")
+    req.set_request_method("DELETE".to_string());
 }
 
+// our entry point into the application
 #[no_mangle]
 pub extern "C" fn handleRequest(ptr: i32, len: i32) {
     whr::run(ptr, len, handler)
